a99d04af3f1f145b8fd6bd48de434d7b4a239227
gre-v-el <95856287+gre-v-el@users.noreply.github.com>
2021-12-12 17:06:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+import * as THREE from 'https://threejs.org/build/three.module.js'
+
+import vertex from './shaders/vertex_glsl.js'
+import fragment from './shaders/fragment_glsl.js'
+
+// three.js setups
+var canvasHTML = document.getElementById('canvas');
+var sceneThree = new THREE.Scene();
+var rendererThree = new THREE.WebGLRenderer({ canvas: canvasHTML, antialias: true });
+var cameraThree = new THREE.PerspectiveCamera(45, canvasHTML.clientWidth / canvasHTML.clientWidth, 1, 1000);
+var clock = new THREE.Clock();
+
+// fps view
+var script = document.createElement('script');
+var stats;
+script.onload = function () {
+	stats = new Stats();
+	document.body.appendChild(stats.dom);
+};
+script.src = '//mrdoob.github.io/stats.js/build/stats.min.js';
+document.head.appendChild(script);
+
+// uniforms for ray marching renderer
+var rmUniforms = {
+	aspect: { value: cameraThree.aspect },
+	time: {value: 0}
+};
+
+// main display setup
+var quadDisplay = new THREE.Mesh(
+	new THREE.PlaneGeometry(2, 2),
+	new THREE.ShaderMaterial({
+		vertexShader: vertex,
+		fragmentShader: fragment,
+		uniforms: rmUniforms,
+		depthWrite: false,
+		depthTest: false
+	})
+);
+sceneThree.add(quadDisplay);
+
+
+
+function tick() {
+	var dt = clock.getDelta();
+
+	rmUniforms.time.value = clock.elapsedTime;
+
+	render();
+	if(stats != undefined) stats.update();
+	requestAnimationFrame(tick);
+}
+
+function render() {
+
+	if (canvasHTML.width !== canvasHTML.clientWidth || canvasHTML.height !== canvasHTML.clientHeight) {
+		rendererThree.setSize(canvasHTML.clientWidth, canvasHTML.clientHeight, false);
+		cameraThree.aspect = canvasHTML.clientWidth / canvasHTML.clientHeight;
+		cameraThree.updateProjectionMatrix();
+		rmUniforms.aspect.value = cameraThree.aspect;
+	}
+	rendererThree.render(sceneThree, cameraThree);
+}
+
+tick();
---
+++ shaders/vertex_glsl.js
@@ -0,0 +1,13 @@
+const vertex = /*glsl*/ ` 
+
+uniform float aspect;
+
+varying vec2 UV;
+
+void main() {
+	UV = (uv - vec2(0.5)) * vec2(aspect, 1);
+	gl_Position = vec4(position.x, position.y, 0, 1.0);
+}
+`;
+
+export default vertex;
